e42c247c188e9ae04261a0fb6733654c344717ef
Mathijs Groothuis <mathijs.groothuis@gmail.com>
2019-11-13 11:35:21 +0100

    Add mongoose to package and edit environment variables accordingly

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = express();
 const request = require("request");
 //const middleware = require("./middleware"); //Implicitly refers to index.js
 
+// Require Mongoose
+const mongoose = require("mongoose");
+
+// Actual DB connection
+mongoose.connect(process.env.DB_FULLPATH, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
 // Set view engine to EJS
 app.set("view engine", "ejs");
 // Defining public folder
