bdc3c67628f51e55931ce75979cf81117971d1cc
Robert de Grijs <rdgrijs@hotmail.com>
2019-11-13 10:52:12 +0100

    add stylesheet

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 // Require env file
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const request = require("request");
+const Product = require("./models/product");
 //const middleware = require("./middleware"); //Implicitly refers to index.js
 
 // Set view engine to EJS
 app.set("view engine", "ejs");
+
 // Defining public folder
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 // ROUTE VARIABLES
 const indexRoutes = require("./routes/index");
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,8 @@ const app = express();
 const router = express.Router({ mergeParams: true });
 
 // MAIN ROUTE TO HOME
-router.get("/", function (req, res)
-{
-    res.render("home");
+router.get("/", function(req, res) {
+  res.render("home");
 });
-
 
 module.exports = router;
