88c5fb29d0cfe6fefb7aa07e97e58513b50ae3d7
Robert de Grijs <rdgrijs@hotmail.com>
2019-11-13 11:23:31 +0100

    add product new route

--- models/product.js
+++ models/product.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
   title: String,
-  description: String
+  body: String
 });
 
 module.exports = mongoose.model("Product", productSchema);
