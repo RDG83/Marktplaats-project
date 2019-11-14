f3f5cb92e0f487ae805ad49e7b7ccbddb028a7eb
Robert de Grijs <rdgrijs@hotmail.com>
2019-11-14 16:31:53 +0100

    make distinct category output index and home

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
 const router = express.Router({ mergeParams: true });
+const Product = require("../models/product");
 
 // MAIN ROUTE TO HOME
 router.get("/", function(req, res) {
-  res.render("home");
+  Product.find({}, function(error, allProducts) {
+    if (error) {
+      console.log(error);
+    } else {
+      let catSet = new Set();
+      allProducts.forEach(function(product) {
+        catSet.add(product.category);
+        console.log(catSet);
+      });
+      res.render("home", { product: catSet });
+    }
+  });
 });
 
 module.exports = router;
