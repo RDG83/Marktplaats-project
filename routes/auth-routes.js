9890a87418391412e67dde48d5815f4495d56f12
Robert de Grijs <rdgrijs@hotmail.com>
2019-11-22 13:45:01 +0100

    add flash message to post signup route

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -26,9 +26,32 @@ router.get("/signup", function(req, res) {
 });
 
 router.post("/signup", function(req, res) {
+  const { name, email, password, password2 } = req.body;
   // passport handling here
-  console.log(req.body);
-  res.send("signup route");
+  let errors = [];
+  // check of alle velden zijn ingevuld
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: "Vul alle velden in" });
+  }
+  // check of passwords hetzelfde zijn
+  if (password !== password2) {
+    errors.push({ msg: "Wachtwoorden komen niet overeen" });
+  }
+  // check of password lang genoeg is
+  if (password.length < 6) {
+    errors.push({ msg: "Wachtwoord is onvoldoende lang" });
+  }
+  if (errors.length > 0) {
+    res.render("signup", {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    });
+  } else {
+    res.send("signup succesvol");
+  }
 });
 
 module.exports = router;
