9046f9649d393de885d6d12a3a523eb5f4748144
Robert de Grijs <rdgrijs@hotmail.com>
2019-11-26 10:24:17 +0100

    add comment post route and comment on show page

--- routes/comment.js
+++ routes/comment.js
@@ -12,4 +12,30 @@ router.get("/new", function(req, res) {
   });
 });
 
+router.post("/", function(req, res) {
+  Product.findById(req.params.product_id, function(err, product) {
+    if (err) {
+      console.log(err);
+      res.redirect("/advertenties");
+    } else {
+      // appendCommentData(product.comments, req.user._id, req.user.username, req.body.content);
+      product.comments.push({ content: req.body.content, author: { username: req.user.username, id: req.user._id } });
+      console.log(product);
+      product.save();
+      res.redirect("/advertenties/" + req.params.product_id);
+    }
+  });
+});
+
+function appendCommentData(targetObject, authorId, username, content) {
+  let comment = {
+    author: {
+      id: { authorId },
+      username: username
+    },
+    content: content
+  };
+  return (targetObject = Object.assign(targetObject, comment));
+}
+
 module.exports = router;
