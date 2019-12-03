93eccbdbd6921217f47a3c08a7e22d5510c538c9
Robert de Grijs <rdgrijs@hotmail.com>
2019-12-03 10:53:59 +0100

    progress on thread limitation

--- routes/message.js
+++ routes/message.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const Product = require("../models/product");
 const Thread = require("../models/thread");
+require('mongoose').mongo.ObjectID;
 
 router.get("/new", function (req, res) {
   Product.findById(req.params.product_id, function (err, foundProduct) {
@@ -14,32 +15,46 @@ router.get("/new", function (req, res) {
 });
 
 router.post("/", isLoggedIn, function (req, res) {
+  let message = req.body.message;
   Product.findById(req.params.product_id, function (err, product) {
     if (err) {
       console.log(err);
       res.redirect("/advertenties");
     } else {
-      Thread.create(req.body.message, function (err, thread) {
-        let message = req.body.message;
-        if (err) {
-          console.log(err);
-        } else {
-          // If logged in, save user data in message
-          if (req.user) {
-            // Assign current user to participant of users array
-            Object.assign(message, req.user);
-            // Assign owner of the product to the array as well
-            thread.users.push(product.author);
-            thread.users.push(req.user);
-            // Reference the newly created thread to the Product threads array
-            product.threads.push(thread.id);
-            product.save();
-            // reference to product in thread
-            thread.product.id = req.params.product_id;
-            // Assign posted message to thread
-            thread.messages.push(message);
-            thread.save();
-          }
+      console.log("prod id: " + typeof product._id)
+      console.log("params: " + typeof req.params.product_id)
+      Thread.find({ "users._id": req.user._id }, function (error, thread) {
+        // Thread.find({ $and: [{ "users._id": req.user._id }, { "product._id": req.params.product_id }] }, function (error, thread) {
+        if (error) {
+          console.log(error)
+        } else if (thread.product._id.equals(product._id)) {
+          res.send(JSON.stringify(thread));
+        }
+        else {
+          // hier komt code voor aanmaken bericht in bestaande thread
+          //thread.messages.push(message);
+          //thread.save();
+          Thread.create(req.body.message, function (err, thread) {
+            if (err) {
+              console.log(err);
+            } else {
+              if (req.user) {
+                // Assign current user to participant of users array
+                Object.assign(message, req.user);
+                // Assign owner of the product to the array as well
+                thread.users.push(product.author);
+                thread.users.push(req.user);
+                // Reference the newly created thread to the Product threads array
+                product.threads.push(thread.id);
+                product.save();
+                // reference to product in thread
+                thread.product.id = req.params.product_id;
+                // Assign posted message to thread
+                thread.messages.push(message);
+                thread.save();
+              }
+            }
+          });
           res.redirect("/advertenties/" + req.params.product_id);
         }
       });
@@ -47,17 +62,17 @@ router.post("/", isLoggedIn, function (req, res) {
   });
 });
 
-router.get("/tonen", function (req, res) {
-  Product.findById(req.params.product_id)
-    .populate("messages")
-    .exec(function (err, product) {
-      if (err || !product) {
-        console.log(err);
-      } else {
-        res.render("messages/index", { product: product, product_id: req.params.product_id });
-      }
-    });
-});
+// router.get("/tonen", function (req, res) {
+//   Product.findById(req.params.product_id)
+//     .populate("messages")
+//     .exec(function (err, product) {
+//       if (err || !product) {
+//         console.log(err);
+//       } else {
+//         res.render("messages/index", { product: product, product_id: req.params.product_id });
+//       }
+//     });
+// });
 
 
 
