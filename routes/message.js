de72b2eaeafe9529c6e8908e82819ad0eb47a1e7
Robert de Grijs <rdgrijs@hotmail.com>
2019-12-03 12:11:16 +0100

    add redirect existing thread route

--- routes/message.js
+++ routes/message.js
@@ -4,70 +4,56 @@ const Product = require("../models/product");
 const Thread = require("../models/thread");
 require('mongoose').mongo.ObjectID;
 
-router.get("/new", function (req, res)
-{
-  Product.findById(req.params.product_id, function (err, foundProduct)
-  {
-    if (err)
-    {
+router.get("/new", function (req, res) {
+  Product.findById(req.params.product_id, function (err, foundProduct) {
+    if (err) {
       console.log(err);
-    } else
-    {
+    } else {
       res.render("messages/new", { product: foundProduct });
     }
   });
 });
 
-router.post("/", isLoggedIn, function (req, res)
-{
+router.post("/", isLoggedIn, function (req, res) {
   // Find product
-  Product.findById(req.params.product_id, function (err, product)
-  {
-    if (err)
-    {
+  Product.findById(req.params.product_id, function (err, product) {
+    if (err) {
       console.log(err);
       res.redirect("/advertenties");
     }
-    else
-    {
-      Thread.findOne({ $and: [{"users._id": req.user._id}, {"product._id": req.params.product_id }]}, function (error, thread)
-      {
+    else {
+      Thread.findOne({ $and: [{ "users._id": req.user._id }, { "product._id": req.params.product_id }] }, function (error, thread) {
         // Thread.find({ $and: [{ "users._id": req.user._id }, { "product._id": req.params.product_id }] }, function (error, thread) {
-        if (error)
-        {
+        if (error) {
           console.log(error)
         }
         // If a thread exists
-        if (thread)
-        {
+        if (thread) {
           // append message to existing thread
           thread.messages.push(req.body.message);
           thread.save();
+          res.redirect("/account/berichten")
         }
         //If no thread exists, make a new thread
-        else
-        {
-          Thread.create(req.body.message, function (error, thread)
-          {
-            if (error)
-            {
+        else {
+          Thread.create(req.body.message, function (error, thread) {
+            if (error) {
               console.log(error);
             }
-            else
-            {
+            else {
               // reference to product in thread
               thread.product._id = req.params.product_id;
               // Assign current user to participant of users array
               Object.assign(req.body.message, req.user);
-              
+
               // Assign owner of the product to the array as well
               thread.users.push(product.author);
               thread.users.push(req.user);
-              
+
               // Reference the newly created thread to the Product threads array
               product.threads.push(thread.id);
               product.save();
-              
+
               // Assign posted message to thread
               thread.messages.push(req.body.message);
               thread.save();
@@ -104,23 +90,16 @@ router.post("/", isLoggedIn, function (req, res)
 //   });
 // });
 
-router.post("/:message_id", function (req, res)
-{
-  Product.findById(req.params.product_id, function (err, product)
-  {
-    if (err)
-    {
+router.post("/:message_id", function (req, res) {
+  Product.findById(req.params.product_id, function (err, product) {
+    if (err) {
       console.log(err);
       res.redirect("/advertenties");
-    } else
-    {
-      Message.create(req.body.message, function (err, message)
-      {
-        if (err)
-        {
+    } else {
+      Message.create(req.body.message, function (err, message) {
+        if (err) {
           console.log(err);
-        } else
-        {
+        } else {
           message.author.id = req.user._id;
           message.author.username = req.user.username;
           message.parentId.id = req.params.message_id
@@ -134,13 +113,10 @@ router.post("/:message_id", function (req, res)
   });
 });
 
-function isLoggedIn(req, res, next)
-{
-  if (req.isAuthenticated())
-  {
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
     return next();
-  } else
-  {
+  } else {
     res.redirect("/auth/login");
   }
 }
