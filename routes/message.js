dac12313efb137ff3c802de8b47f8f696de65867
Mathijs Groothuis <mathijs.groothuis@gmail.com>
2019-12-03 12:03:56 +0100

    Add working thread functionality

--- routes/message.js
+++ routes/message.js
@@ -4,55 +4,73 @@ const Product = require("../models/product");
 const Thread = require("../models/thread");
 require('mongoose').mongo.ObjectID;
 
-router.get("/new", function (req, res) {
-  Product.findById(req.params.product_id, function (err, foundProduct) {
-    if (err) {
+router.get("/new", function (req, res)
+{
+  Product.findById(req.params.product_id, function (err, foundProduct)
+  {
+    if (err)
+    {
       console.log(err);
-    } else {
+    } else
+    {
       res.render("messages/new", { product: foundProduct });
     }
   });
 });
 
-router.post("/", isLoggedIn, function (req, res) {
-  let message = req.body.message;
-  Product.findById(req.params.product_id, function (err, product) {
-    if (err) {
+router.post("/", isLoggedIn, function (req, res)
+{
+  // Find product
+  Product.findById(req.params.product_id, function (err, product)
+  {
+    if (err)
+    {
       console.log(err);
       res.redirect("/advertenties");
-    } else {
-      console.log("prod id: " + typeof product._id)
-      console.log("params: " + typeof req.params.product_id)
-      Thread.find({ "users._id": req.user._id }, function (error, thread) {
+    }
+    else
+    {
+      Thread.findOne({ $and: [{"users._id": req.user._id}, {"product._id": req.params.product_id }]}, function (error, thread)
+      {
         // Thread.find({ $and: [{ "users._id": req.user._id }, { "product._id": req.params.product_id }] }, function (error, thread) {
-        if (error) {
+        if (error)
+        {
           console.log(error)
-        } else if (thread.product._id.equals(product._id)) {
-          res.send(JSON.stringify(thread));
         }
-        else {
-          // hier komt code voor aanmaken bericht in bestaande thread
-          //thread.messages.push(message);
-          //thread.save();
-          Thread.create(req.body.message, function (err, thread) {
-            if (err) {
-              console.log(err);
-            } else {
-              if (req.user) {
-                // Assign current user to participant of users array
-                Object.assign(message, req.user);
-                // Assign owner of the product to the array as well
-                thread.users.push(product.author);
-                thread.users.push(req.user);
-                // Reference the newly created thread to the Product threads array
-                product.threads.push(thread.id);
-                product.save();
-                // reference to product in thread
-                thread.product.id = req.params.product_id;
-                // Assign posted message to thread
-                thread.messages.push(message);
-                thread.save();
-              }
+        // If a thread exists
+        if (thread)
+        {
+          // append message to existing thread
+          thread.messages.push(req.body.message);
+          thread.save();
+        }
+        //If no thread exists, make a new thread
+        else
+        {
+          Thread.create(req.body.message, function (error, thread)
+          {
+            if (error)
+            {
+              console.log(error);
+            }
+            else
+            {
+              // reference to product in thread
+              thread.product._id = req.params.product_id;
+              // Assign current user to participant of users array
+              Object.assign(req.body.message, req.user);
+              
+              // Assign owner of the product to the array as well
+              thread.users.push(product.author);
+              thread.users.push(req.user);
+              
+              // Reference the newly created thread to the Product threads array
+              product.threads.push(thread.id);
+              product.save();
+              
+              // Assign posted message to thread
+              thread.messages.push(req.body.message);
+              thread.save();
             }
           });
           res.redirect("/advertenties/" + req.params.product_id);
@@ -86,16 +104,23 @@ router.post("/", isLoggedIn, function (req, res) {
 //   });
 // });
 
-router.post("/:message_id", function (req, res) {
-  Product.findById(req.params.product_id, function (err, product) {
-    if (err) {
+router.post("/:message_id", function (req, res)
+{
+  Product.findById(req.params.product_id, function (err, product)
+  {
+    if (err)
+    {
       console.log(err);
       res.redirect("/advertenties");
-    } else {
-      Message.create(req.body.message, function (err, message) {
-        if (err) {
+    } else
+    {
+      Message.create(req.body.message, function (err, message)
+      {
+        if (err)
+        {
           console.log(err);
-        } else {
+        } else
+        {
           message.author.id = req.user._id;
           message.author.username = req.user.username;
           message.parentId.id = req.params.message_id
@@ -109,10 +134,13 @@ router.post("/:message_id", function (req, res) {
   });
 });
 
-function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
+function isLoggedIn(req, res, next)
+{
+  if (req.isAuthenticated())
+  {
     return next();
-  } else {
+  } else
+  {
     res.redirect("/auth/login");
   }
 }
