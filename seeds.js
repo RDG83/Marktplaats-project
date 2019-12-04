dde6261afb3794126c8c7122bd071251abf70966
Robert de Grijs <rdgrijs@hotmail.com>
2019-12-04 11:00:12 +0100

    add seed for thread

--- seeds.js
+++ seeds.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Product = require("./models/product");
+const Thread = require("./models/thread")
 
 function seedDB() {
     // remove products
@@ -17,6 +18,15 @@ function seedDB() {
                 console.log(err);
             } else {
                 console.log("added product");
+            }
+        })
+    })
+    threadData.forEach(function (thread) {
+        Thread.create(thread, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("added thread")
             }
         })
     })
@@ -39,7 +49,7 @@ const seedData =
             location: {
                 coordinates: [53.218734715241844, 6.567541658878327]
             },
-            threads: [],
+            threads: ["5de641b83e284d31f8e016fb"],
             bids: [
                 { "_id": "5de770dd72b7d42bbc15d30e", "amount": 250, "createdAt": "2019-12-04T08:39:57.155Z", "author": { "username": "Mathijs" } },
                 { "_id": "5de7710672b7d42bbc15d311", "amount": 260, "createdAt": "2019-12-04T08:40:38.500Z", "author": { "username": "Kees" } },
@@ -158,5 +168,32 @@ const seedData =
             }
         }
     ];
+
+const threadData = [
+    {
+        "_id": "5de641b83e284d31f8e016fb",
+        "users": [
+            { "_id": "5de61ea14fd6ae1c5c7d918f", "username": "Mathijs" },
+            { "_id": "5de61e8e4fd6ae1c5c7d918e", "username": "Robert" }
+        ],
+        "messages": [
+            {
+                "_id": "5de641b83e284d31f8e016fc",
+                "content": "        Beste Mathijs,\r\n  \r\nIk heb een vraag over 'Product Mathijs 1'\r\n  \r\n  hoi mathijs\r\n  \r\nMet vriendelijke groet,\r\n  \r\nRobert\r\n  ",
+                "createdAt": "2019-12-03T11:06:32.875Z"
+            },
+            {
+                "_id": "5de641e73e284d31f8e016fd",
+                "content": "        Beste Mathijs,\r\n  \r\nIk heb een vraag over 'Product Mathijs 1'\r\n  \r\n  nog een keer hoi mathijs\r\n  \r\nMet vriendelijke groet,\r\n  \r\nRobert\r\n  ",
+                "createdAt": "2019-12-03T11:07:19.887Z"
+            },
+            {
+                "_id": "5de642327d53ed376c3450a5",
+                "content": "        Beste Mathijs,\r\n  \r\nIk heb een vraag over 'Product Mathijs 1'\r\n  \r\n  bericht 3\r\n  \r\nMet vriendelijke groet,\r\n  \r\nRobert\r\n  ",
+                "createdAt": "2019-12-03T11:08:34.088Z"
+            }], "__v": 3,
+        "product": { "_id": "5de61eb74fd6ae1c5c7d9190" }
+    }
+]
 
 module.exports = seedDB
