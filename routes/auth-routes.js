2ef1e5dc2dd12a51efe1709434f0098d2b4a6f0d
Mathijs Groothuis <mathijs.groothuis@gmail.com>
2019-12-11 14:06:26 +0100

    Alter signup and login page to be more consistent

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -3,9 +3,10 @@ const router = express.Router({ mergeParams: true });
 const User = require("../models/user");
 const passport = require("passport");
 
-router.get("/login", function (req, res) {
+router.get("/login", function (req, res)
+{
   // passport handling here
-  res.render("login");
+  res.render("accounts/login");
 });
 
 router.post(
@@ -18,61 +19,78 @@ router.post(
   })
 );
 
-router.get("/logout", function (req, res) {
+router.get("/logout", function (req, res)
+{
   req.logout();
   res.redirect("/advertenties");
 });
 
-router.get("/signup", function (req, res) {
-  res.render("signup");
+router.get("/signup", function (req, res)
+{
+  res.render("accounts/signup");
 });
 
-router.post("/signup", function (req, res) {
+router.post("/signup", function (req, res)
+{
   const { username, email, password, password2 } = req.body;
   let errors = [];
   // check of alle velden zijn ingevuld
-  if (!username || !email || !password || !password2) {
+  if (!username || !email || !password || !password2)
+  {
     errors.push({ msg: "Vul alle velden in" });
   }
   // check of passwords hetzelfde zijn
-  if (password !== password2) {
+  if (password !== password2)
+  {
     errors.push({ msg: "Wachtwoorden komen niet overeen" });
   }
   // check of password lang genoeg is
-  if (password.length < 6) {
+  if (password.length < 6)
+  {
     errors.push({ msg: "Wachtwoord is onvoldoende lang" });
   }
-  if (errors.length > 0) {
-    res.render("signup", {
+  if (errors.length > 0)
+  {
+    res.render("accounts/signup", {
       errors,
       username,
       email,
       password,
       password2
     });
-  } else {
-    User.findOne({ email: email }).then(function (user) {
-      if (user) {
+  } else
+  {
+    User.findOne({ email: email }).then(function (user)
+    {
+      if (user)
+      {
         errors.push({ msg: "Email is al in gebruik" });
-        res.render("signup", {
+        res.render("accounts/signup", {
           errors,
           username,
           email,
           password,
           password2
         });
-      } else {
+      } else
+      {
         const newUser = new User({
           username,
           password,
           email
         });
-        User.register(newUser, req.body.password, function (err, user) {
-          if (err) {
+        User.register(newUser, req.body.password, function (err, user)
+        {
+          if (err)
+          {
             console.log("post route error:" + err);
-            return res.render("signup");
-          } else {
-            passport.authenticate("local")(req, res, function () {
+            return res.render("accounts/signup");
+          }
+          else
+          {
+            passport.authenticate("local")(req, res, function ()
+            {
+              req.flash("success", "Succesvol geregistreerd op de website.");
               res.redirect("/advertenties");
             });
           }
